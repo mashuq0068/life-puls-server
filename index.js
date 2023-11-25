a01689b5b97edb12dfea09825d39d16fc8eb8bc0
mashuq0068 <mashuq0068@gmail.com>
2023-11-25 23:23:08 +0600

    added the put api of biodata

--- index.js
+++ index.js
@@ -44,10 +44,12 @@ async function run() {
     const bioDataCollection = database.collection("biodatas")
    app.post('/biodata', async(req , res) => {
     const biodata = req.body
-    const query = {biodataId : biodata?.biodataId}
-    const isExisted =await bioDataCollection.findOne(query)
-    if(isExisted){
+    // const query = {biodataId : biodata?.biodataId}
+    // console.log(query)
+    // const isExisted =await bioDataCollection.findOne(query)
+    if( biodata?.biodataId){
       const filter = {biodataId : biodata?.biodataId}
+      console.log(filter)
       const options = { upsert: true }
       const updateDoc = {
         $set: {
@@ -76,12 +78,23 @@ async function run() {
     res.send(updateResult)
     return;
    }
-   const biodataCount = await bioDataCollection.estimatedDocumentCount()
-   console.log(biodataCount)
-   if(biodataCount && !biodata?.biodataId){
-     biodata.biodataId = biodataCount + 1
-     res.send(biodata?.biodataId)
-   }
+  //  const biodataCount = await bioDataCollection.estimatedDocumentCount()
+  //  console.log(biodataCount)
+  //  if(biodataCount){
+  //    biodata.biodataId = biodataCount + 1
+  //    res.send(biodata?.biodataId)
+  //  }
+
+
+
+//   const filter = {biodataId : biodata?.biodataId}
+//   console.log(filter)
+//   const options = { upsert: true }
+//   const updateDoc = {
+//     $set: {
+     
+//    }
+// }
    
    
     const postResult =await bioDataCollection.insertOne(biodata)
@@ -90,7 +103,43 @@ async function run() {
 
    })
     // await client.connect();
+app.put('/biodata', async (req , res)=>{
+  const biodata = req.body
+  const filter = {email : biodata?.email}
+  const biodataCount = await bioDataCollection.estimatedDocumentCount()
+  const query = {email : biodata?.email}
+  const existedBiodata = await bioDataCollection.findOne(query)
+   console.log(biodataCount)
+  console.log(filter)
+  const options = { upsert: true }
 
+  const updateDoc = {
+    $set: {
+      biodataId : existedBiodata?.biodataId || biodataCount + 1,
+      DateOfBirth:biodata?.DateOfBirth,
+      age:biodata?.age,
+      biodataType:biodata?.biodataType,
+      division:biodata?.division,
+      email:biodata?.email,
+      expectedPartnerAge:biodata?.expectedPartnerAge,
+      expectedPartnerHeight:biodata?.expectedPartnerHeight,
+      expectedPartnerWeight:biodata?.expectedPartnerWeight,
+      fathersName:biodata?.fathersName,
+      height:biodata?.height,
+      mobileNumber:biodata?.mobileNumber,
+      mothersName:biodata?.mothersName,
+      name:biodata?.name,
+      occupation:biodata?.occupation,
+      profileLink:biodata?.profileLink,
+      race:biodata?.race,
+      weight:biodata?.weight
+}
+}
+const updateResult = await bioDataCollection.updateOne(filter, updateDoc, options)
+res.send(updateResult)
+
+
+})
 
    
 
